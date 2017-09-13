05c9c57f3ce663133e731916c21f284d48f50ebd
Nuno <nunonuninho2@gmail.com>
2017-09-13 01:10:53 +0100

    Removed unused files

---
+++ Multipliers/src/main/java/com/nuno1212s/multipliers/main/Main.java
@@ -0,0 +1,7 @@
+package com.nuno1212s.multipliers.main;
+
+/**
+ * Created by COMP on 20/07/2017.
+ */
+public class Main {
+}
---
+++ Multipliers/src/main/java/com/nuno1212s/multipliers/rankmultipliers/MultipliersManager.java
@@ -0,0 +1,7 @@
+package com.nuno1212s.multipliers.rankmultipliers;
+
+/**
+ * Created by COMP on 20/07/2017.
+ */
+public class MultipliersManager {
+}
