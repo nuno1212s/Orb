f644962eafe368b598dbd2c608dd9a76f9681965
Nuno <nunonuninho2@gmail.com>
2017-09-13 01:11:46 +0100

    More hub stuff

--- Multipliers/src/main/java/com/nuno1212s/multipliers/main/Main.java
+++
@@ -1,7 +0,0 @@
-package com.nuno1212s.multipliers.main;
-
-/**
- * Created by COMP on 20/07/2017.
- */
-public class Main {
-}
--- Multipliers/src/main/java/com/nuno1212s/multipliers/rankmultipliers/MultipliersManager.java
+++
@@ -1,7 +0,0 @@
-package com.nuno1212s.multipliers.rankmultipliers;
-
-/**
- * Created by COMP on 20/07/2017.
- */
-public class MultipliersManager {
-}
